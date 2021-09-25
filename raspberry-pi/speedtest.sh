7248c80ccd7946cc6127b16743edfbbf26bd87fe
Keita Morisaki <kmta1236@gmail.com>
2021-09-25 23:02:34 +0900

    GAS URLの説明を追加

--- raspberry-pi/speedtest.sh
+++ raspberry-pi/speedtest.sh
@@ -16,6 +16,7 @@ echo $upload
 result="$timestamp,$ping,$download,$upload"
 
 # post to google apps script
-# SPEEDTEST_LOGGER_URL should be set in bash
+# SPEEDTEST_LOGGER_URL (web app URL of Google Apps Script) should be set in bash
+# SPEEDTEST_LOGGER_URL="https://script.google.com/macros/s/xxx/exec"
 json="'{\"data\": \"$result\"}'"
 eval "curl --data ${json} --header 'Content-Type: application/json' -L $SPEEDTEST_LOGGER_URL"
